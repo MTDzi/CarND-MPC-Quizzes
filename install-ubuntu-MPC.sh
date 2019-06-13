2d80c4ab6eb8117f7e9ef11c9d979965346831fc
MTDzi <mtdziubinski@gmail.com>
2019-06-13 21:45:10 +0200

    Added -y to all apt install commands

--- install-ubuntu-MPC.sh
+++ install-ubuntu-MPC.sh
@@ -4,14 +4,14 @@
 sudo apt-get update
 
 # gfortran dependency
-sudo apt-get install gfortran
+sudo apt-get install -y gfortran
 
 # get unzip
-sudo apt-get install unzip
+sudo apt-get install -y unzip
 
 # Ipopt: get, install, unzip
 wget https://www.coin-or.org/download/source/Ipopt/Ipopt-3.12.7.zip && unzip Ipopt-3.12.7.zip && rm Ipopt-3.12.7.zip
 ./install_ipopt.sh ./Ipopt-3.12.7
 
 # CppAD
-sudo apt-get install cppad
+sudo apt-get install -y cppad
